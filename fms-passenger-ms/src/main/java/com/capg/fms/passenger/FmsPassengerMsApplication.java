1ce6df38e736164b195ce0de57957e671fe4b0ee
Nag <Nag@LAPTOP-9SJMQIUO>
2020-08-20 09:39:46 +0530

    added eureka server

--- fms-passenger-ms/src/main/java/com/capg/fms/passenger/FmsPassengerMsApplication.java
+++ fms-passenger-ms/src/main/java/com/capg/fms/passenger/FmsPassengerMsApplication.java
@@ -3,7 +3,9 @@ package com.capg.fms.passenger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableAutoConfiguration
+@EnableEurekaClient
 @SpringBootApplication
 public class FmsPassengerMsApplication {
 
